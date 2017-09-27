00d584179baa792c5eaa9f48d15dbcda1a4caf0a
Yevhen Dmitrievskiy <ravenio@ex.ua>
2017-09-27 05:26:42 +0300

    project setup

--- Portfolio_project/source/js/common.js
+++ Portfolio_project/source/js/common.js
@@ -1,8 +1,24 @@
-$(function() {
-	console.log
-	// Custom JS d
-	//some function
+//класс занимает высоту экрана
+$(document).ready(function() {
+  function heightAligment() {
+    $('.main-header').css('height',$(window).height());
+  };
+  //при изменении размера окна занимает область просмотра
+  heightAligment();
+  $(window).resize(function () {
+    heightAligment();
+  });
 
+  $('.main-header').parallax({imageSrc:'../img/bg-dark-grey-brickwall.jpg'});//добавляем параллакс
 });
+
+$(window).on('load',function() {
+  $('.loader-outter').delay(200).fadeIn('slow');
+  $('.loader-inner').delay(200).fadeIn('slow');
+  $('.loader-outter').delay(50).fadeOut('slow');
+  $('.loader-inner').delay(50).fadeOut('slow');
+});
+
+
 
 
