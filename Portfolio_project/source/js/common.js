8f9ae75e298c2752c7b0def049d37c22df53a499
Yevhen Dmitrievskiy <ravenio@ex.ua>
2017-09-28 22:57:22 +0300

    interactive menu was added

--- Portfolio_project/source/js/common.js
+++ Portfolio_project/source/js/common.js
@@ -8,14 +8,31 @@ $(document).ready(function() {
   $(window).resize(function () {
     heightAligment();
   });
+  //добавляем параллакс
+  $('.main-header').parallax({imageSrc:'img/bg-dark-grey-brickwall-1.jpg'});
 
-  $('.main-header').parallax({imageSrc:'../img/bg-dark-grey-brickwall.jpg'});//добавляем параллакс
+  //анимация  меню
+  $('.main-nav__btn').click(function() {
+    $('.main-nav__btn').toggleClass('main-nav__sandwich--active');
+  });
+
+  $('.main-nav__btn').click(function() {
+    if ($('.main-nav__list--wrapper').is(':visible')) {
+      $('.main-nav__list--wrapper').fadeOut(800);
+    } else {
+      $('.main-nav__list--wrapper').fadeIn(800);
+    }
+  });
 });
 
 $(window).on('load',function() {
+  $('.loader').delay(300).fadeIn('slow');
+  $('.loader-text').delay(200).fadeIn('slow');
   $('.loader-outter').delay(200).fadeIn('slow');
   $('.loader-inner').delay(200).fadeIn('slow');
-  $('.loader-outter').delay(50).fadeOut('slow');
+  $('.loader').delay(300).fadeOut('slow');
+  $('.loader-text').delay(250).fadeOut('slow');
+  $('.loader-outter').delay(250).fadeOut('slow');
   $('.loader-inner').delay(50).fadeOut('slow');
 });
 
