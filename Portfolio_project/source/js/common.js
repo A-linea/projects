335e188f9bb5e28c1fdbc6b22ecba12799b582e5
Yevhen Dmitrievskiy <ravenio@ex.ua>
2017-09-29 04:34:55 +0300

    add section abt&portfolio

--- Portfolio_project/source/js/common.js
+++ Portfolio_project/source/js/common.js
@@ -8,20 +8,30 @@ $(document).ready(function() {
   $(window).resize(function () {
     heightAligment();
   });
+
   //добавляем параллакс
   $('.main-header').parallax({imageSrc:'img/bg-dark-grey-brickwall-1.jpg'});
 
-  //анимация  меню
+  //анимация  бургера
   $('.main-nav__btn').click(function() {
     $('.main-nav__btn').toggleClass('main-nav__sandwich--active');
   });
 
+//анимация  меню
   $('.main-nav__btn').click(function() {
     if ($('.main-nav__list--wrapper').is(':visible')) {
-      $('.main-nav__list--wrapper').fadeOut(800);
+      $('.main-nav__list--wrapper').fadeOut(600);
+      $('.main-nav__link').removeClass('slideInUp animated');
     } else {
-      $('.main-nav__list--wrapper').fadeIn(800);
+      $('.main-nav__list--wrapper').fadeIn(600);
+      $('.main-nav__link').addClass('slideInUp animated');
     }
+  });
+
+  //Скрываем меню при клике на ссылку
+  $('.main-nav__link').click(function () {
+    $('.main-nav__list--wrapper').fadeOut(600);
+    $('.main-nav__btn').toggleClass('main-nav__sandwich--active');
   });
 });
 
