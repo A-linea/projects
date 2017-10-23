cece37292fcd14cea6f53ad8476ee1d172596cce
Yevhen Dmitrievskiy <ravenio@ex.ua>
2017-10-24 00:57:49 +0300

    section fav added

--- Bakery/source/js/common.js
+++ Bakery/source/js/common.js
@@ -1,5 +1,27 @@
 $( document ).ready(function() {
 
+   $('.favourite__carousel').owlCarousel({
+     navText: ['<button class="favourite__carousel-nav--right"></button>', '<button class="favourite__carousel-nav--left"></button>'],
+     responsiveClass: true,
+     loop:true,
+     smartSpeed: 700,
+     nav:true,
+     margin: 0,
+     animateIn: true,
+     responsive:{
+       0:{
+         items:1
+       },
+       600:{
+         items:3
+       },
+       1000:{
+         items:3
+       }
+     }
+
+  });
+
 });
 
 
