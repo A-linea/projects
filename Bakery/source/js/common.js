6e13d379b02737378a8691d73c8b13f4a750de92
Yevhen Dmitrievskiy <ravenio@ex.ua>
2017-10-26 22:50:35 +0300

    ready

--- Bakery/source/js/common.js
+++ Bakery/source/js/common.js
@@ -2,18 +2,21 @@ $( document ).ready(function() {
 
    $('.favourite__carousel').owlCarousel({
      navText: ['<button class="favourite__carousel-nav--right"></button>', '<button class="favourite__carousel-nav--left"></button>'],
-     responsiveClass: true,
      loop:true,
      smartSpeed: 700,
-     nav:true,
      margin: 0,
-     animateIn: true,
+     responsiveClass: true,
      responsive:{
        0:{
-         items:1
+         items:2,
+         nav: false
        },
        600:{
          items:3
+       },
+       700:{
+         items:3,
+         nav: true
        },
        1000:{
          items:3
@@ -21,7 +24,11 @@ $( document ).ready(function() {
      }
 
   });
-
+  $('.main-nav__toggle').click(function () {
+    $(this).toggleClass('on');
+    $('.main-nav').slideToggle();
+    return false;
+  });
 });
 
 
